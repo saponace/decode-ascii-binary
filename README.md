2f930b8b1fa67c7fb5da68370d16cde65f6f4048
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2016-09-14 16:11:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Decode a file containing binary representation of ASCII text written in plain text.
+
+The file looks like :
+>01001000011001010110110001101100011011110010000001010111011011110111001001101100011001000010000000100001
+
+The output of this script would be :
+>Hello World !
---
+++ decode_ascii_binary.py
@@ -0,0 +1,22 @@
+from curses import ascii
+
+
+def bin_to_integer(string):
+    ret_val = 0
+    for i in range(len(string)):
+        if (string[i] == "1"):
+            ret_val += 2**(7-i)
+    return ret_val
+
+
+filepath = "/home/saponace/decode-nolinebreak.txt"
+with open(filepath) as buffer:
+    output = ""
+    oct = "beginning"
+    while oct != "":
+        oct = buffer.read(8)
+        char = ascii.unctrl(bin_to_integer(oct))
+        output += char
+    print(output)
+
+
